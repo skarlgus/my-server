a6c13df1557127967be076e71803de95a0cab203
namgihyeon <skarlgus0419@naver.com>
2024-03-24 23:10:55 +0900

    [feat] webconfig 인터셉터 설정 추가

--- src/main/java/com/kh/server/config/WebConfig.java
+++ src/main/java/com/kh/server/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.kh.server.config;
 
 import com.kh.server.formatter.DecryptFormatter;
+import com.kh.server.interceptor.ServiceApiInterceptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
@@ -14,9 +16,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = {"com.kh.server"})
 public class WebConfig implements WebMvcConfigurer {
 
+    private final ServiceApiInterceptor serviceApiInterceptor;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        log.info("@@@@@@@@");
         registry.addFormatterForFieldAnnotation(new DecryptFormatter(""));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(serviceApiInterceptor)
+                .addPathPatterns("/**");
     }
 }
