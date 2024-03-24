1145bf40ebe3dec251e6e2cd5dace430c4a545f5
namgihyeon <skarlgus0419@naver.com>
2024-03-24 23:11:18 +0900

    [test] 로깅 테스트용 end-point 추가

--- src/main/java/com/kh/server/temp/TempController.java
+++ src/main/java/com/kh/server/temp/TempController.java
@@ -1,8 +1,11 @@
 package com.kh.server.temp;
 
+import com.kh.server.formatter.User;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,5 +15,10 @@ public class TempController {
     public String index(HttpServletRequest httpServletRequest) {
         //test-main
         return "현재 서버 포트 ::: "+httpServletRequest.getLocalPort();
+    }
+
+    @PostMapping(value = "/logging")
+    public void logging(@RequestBody User user){
+        //return "logging post response data";
     }
 }
